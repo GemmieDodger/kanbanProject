80236944f4e8787aab26806f6d3822b25116a0ac
devashish-acharya <thedev96@gmail.com>
2020-10-15 14:17:49 +0100

    landing page

--- public/main.js
+++ public/main.js
@@ -1,39 +1,71 @@
+class Project {
+}
 const state = {
-    projects: []
+    projects: [],
+    dragging: null
 }
 
-// instead of => { return our_html }
+const viewProject = project => {
+    return `<li
+    id="${project.id}"
+    draggable="true"
+        onclick="app.run('done', ${project.id})"
+        ondragstart="app.run('onDragProject', event)
+        class="${project.status === 0 ? '' : 'done'}"
+        >${project.text}
+    </li>`
+}
 const view = (state) => `
-    <header>Projects</header>
-    <div>
-    <form onsubmit="app.run('add', this);return false;">
-        <input name="project" placeholder="Make a new project" />
-        <button class="addBtn">Add</button>
-    </form>
-</div>
-</div>
-
-    <div class="row">
-    <div class="column">
-        <h1>project</h1>
+    <section>
+        <h1>Projects</h1>
+       
         <ul>
-            ${state.projects.map(task => `<li id="${task.id}" draggable="true" ondragstart="app.run('dragFromToDoTask', event)">${task.text}</li>`).join("")}
-        </ul>
-      </div>`
-
-      const update = {
-        add: (state, form) => {
-            // access data in a form
-            const data = new FormData(form)
-            const task = {
-                id: window.crypto.getRandomValues(new Uint8Array(3)).join(""),
-                text: data.get('task'),
-                status: 0
+         ${state.projects.map(project => `<li>${project.text}</li>`).join("")} ${state.projects.map(project => {
+            if (project.status === 0){
+                return `<button onclick="app.run('done', ${project.id})">✅</button>`
+            } else {
+                return `<button onclick="app.run('delete', ${project.id})">❌</button>`
             }
-            state.todotasks.push(task)
-            return state
+        })}
+    </ul>
+   
+    </section>
+    <section>
+        <form onsubmit="app.run('add', this);return false;">
+            <input name="project" placeholder="add a project" required/>
+            <button class="addBtn">Add</button>
+        </form>
+     
+    </section>
+`
+const update = {
+    add: (state, form) => {
+        const data = new FormData(form)
+        const project = {
+            id: window.crypto.getRandomValues(new Uint8Array(3)).join(""),
+            text: data.get('project'),
+            status: 0
         }
+        state.projects.push(project)
+        return state
+    },
+    done: (state, id) => {
+        const project = state.projects.find(project => {
+            return project.id == id
+        })
+        project.status = 1
+        console.log(project) 
+        return state
+    },
+    delete: (state, id) => {
+        const project = state.projects.findIndex(project => {
+            return project.id == id
+        })
+        state.projects.splice(project, 1)
+        return state
     }
-    
+}
+
+
 
 app.start('KanbanProject', state, view, update)
