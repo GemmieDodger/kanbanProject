3dfe720be22e4894a8cd2dcb4373911044a9799e
gemmiedodger <gemmadruce@live.co.uk>
2020-10-15 19:41:48 +0100

    split lists into to do, doing and done. Delete works for done. Need to remove delete buttons from Doing/Todo

--- public/lists.js
+++ public/lists.js
@@ -28,48 +28,49 @@ const state = {
     dragging: null
 }
 
-const viewToDoTask = task => {
+const viewTask = task => {
     //split into a new function to show the viewing of task
     return `<li
     id="${task.id}"
     draggable="true" 
     onclick="app.run('done', ${task.id})"
     ondragstart="app.run('onDragTask', event)"
-    >${task.text}
+    class="${task.status === 0 ? '' : 'done'}"
+    >${task.text} 
         <button class="deleteButton" onclick="app.run('deleteTask', ${task.id})">Delete</button>
     </li>
     `
-}
-const viewDoingTask = task => {
-    //split into a new function to show the viewing of task
-    return `<li
-    id="${task.id}"
-    draggable="true" 
-    onclick="app.run('done', ${task.id})"
-    ondragstart="app.run('onDragTask', event)"
-    >${task.text}
-        <button class="deleteButton" onclick="app.run('deleteTask', ${task.id})">Delete</button>
-    </li>
-    `
-}
-const viewDoneTask = task => {
-    //split into a new function to show the viewing of task
-    return `<li
-    id="${task.id}"
-    draggable="true" 
-    onclick="app.run('done', ${task.id})"
-    ondragstart="app.run('onDragTask', event)"
-    >${task.text}
-        <button class="deleteButton" onclick="app.run('deleteTask', ${task.id})">Delete</button>
-    </li>
-    `
-}
+    }
+// const viewDoingTask = task => {
+//     //split into a new function to show the viewing of task
+//     return `<li
+//     id="${task.id}"
+//     draggable="true" 
+//     onclick="app.run('done', ${task.id})"
+//     ondragstart="app.run('onDragTask', event)"
+//     >${task.text}
+//         <button class="deleteButton" onclick="app.run('deleteTask', ${task.id})">Delete</button>
+//     </li>
+//     `
+// }
+// const viewDoneTask = task => {
+//     //split into a new function to show the viewing of task
+//     return `<li
+//     id="${task.id}"
+//     draggable="true" 
+//     onclick="app.run('done', ${task.id})"
+//     ondragstart="app.run('onDragTask', event)"
+//     >${task.text}
+//         <button class="deleteButton" onclick="app.run('deleteTask', ${task.id})">Delete</button>
+//     </li>
+//     `
+// }
 
 const view = (state) => 
      `<section>
         <h2>To do list</h2>
         <ul>
-            ${state.toDoTasks.map(viewToDoTask).join("")}
+            ${state.toDoTasks.map(viewTask).join("")}
         </ul>
         <form onsubmit="app.run('add', this);return false;">
             <input name="task" placeholder="add a task" />
@@ -80,17 +81,17 @@ const view = (state) =>
     <section >
         <h2>Doing</h2>
         <ul>
-            ${state.doingTasks.map(viewDoingTask).join("")}
+            ${state.doingTasks.map(viewTask).join("")}
         </ul>
     </section>
 
     <section >
         <h2>Done</h2>
         <ul>
-            ${state.doneTasks.map(viewDoneTask).join("")}
+            ${state.doneTasks.map(viewTask).join("")}
         </ul>
         <button>Delete</button>
-        <div class="deleteOnHover" ondragover="event.preventDefault()" ondrop="app.run('onDropDeleteInProcessTask', event)">Delete</div>  
+        <div class="deleteOnHover" ondragover="event.preventDefault()" ondrop="app.run('onDropDeleteTask', event)">Delete</div>  
     </section>
     `
 
@@ -107,16 +108,18 @@ const update = {
             },
             body: JSON.stringify(task)
         }
-        fetch('/tasks', postRequest).then(() => app.run('getToDoTasks'))
-        fetch('/tasks', postRequest).then(() => app.run('getDoingTasks'))
-        fetch('/tasks', postRequest).then(() => app.run('getDoneTasks'))
+        console.log("new task")
+        console.log(task) //this is right
+        console.log("to do list tasks")
+        console.log(state.toDoTasks) //this is right
+        fetch('/toDoTasks', postRequest).then(() => app.run('getTasks'))
         return state
     },
 
     deleteTask: (state, id) => {
     
-        const index = state.tasks.findIndex(element=> element.id === id)
-        state.tasks.splice(index,1)
+        const index = state.doneTasks.findIndex(element=> element.id === id)
+        state.doneTasks.splice(index,1)
         return state
     },
 
@@ -125,30 +128,34 @@ const update = {
         return state
     },
 
-    onDropTask: (state, event) => {
+    onDropDeleteTask: (state, event) => { //THIS IS WORKING
         const id = event.dataTransfer.getData('text')
         console.log(id)
-        const index = state.tasks.findIndex(task => task.id == id)
-        state.tasks.splice(index,1)
+        const index = state.doneTasks.findIndex(task => task.id == id)
+        state.doneTasks.splice(index,1)
         return state
     },
     getTasks: async (state) => {
 
-        state.toDoTasks = await fetch('/tasks').then(res => res.json())
-        state.doingTasks = await fetch('/tasks').then(res => res.json())
-        state.doneTasks = await fetch('/tasks').then(res => res.json())
-        console.log("to do")
-        console.log(state.toDoTasks)
-        console.log("tdoing")
-        console.log(state.doingTasks)
-        console.log("done")
-        console.log(state.doneTasks)
+        state.toDoTasks = await fetch('/toDoTasks').then(res => res.json())
+        // console.log("to do")
+        // console.log(state.toDoTasks)
+        // console.log("tdoing")
+        // console.log(state.doingTasks)
+        // console.log("done")
+        // console.log(state.doneTasks)
         return state
     },
-    
+    getAllTasks: async (state) => {
+        state.toDoTasks = await fetch('/toDoTasks').then(res => res.json())
+        state.doingTasks = await fetch('/doingtasks').then(res => res.json())
+        state.doneTasks = await fetch('/doneTasks').then(res => res.json())
+        return state
+    },
 
-    addTasks: (state, tasks) => { 
-        state.tasks = {...state.tasks, ...tasks}
+    addTasks: (state, toDoTasks) => { 
+        state.toDoTasks = {...state.toDoTasks, ...toDoTasks}
+        console.log(toDoTasks)
         return state //three dots remove from array concats to antoher array
     }
 
@@ -156,4 +163,4 @@ const update = {
 
 
 app.start('listView', state, view, update)
-app.get('getTasks')
+app.run('getAllTasks')
