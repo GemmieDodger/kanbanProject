6fda3e0ba3e283a4776dad32011a068c14d99eb1
devashish-acharya <thedev96@gmail.com>
2020-10-15 11:32:46 +0100

    working server

--- public/main.js
+++ public/main.js
@@ -10,7 +10,30 @@ const view = (state) => `
         <input name="project" placeholder="Make a new project" />
         <button class="addBtn">Add</button>
     </form>
-</div>`
+</div>
+</div>
 
+    <div class="row">
+    <div class="column">
+        <h1>project</h1>
+        <ul>
+            ${state.projects.map(task => `<li id="${task.id}" draggable="true" ondragstart="app.run('dragFromToDoTask', event)">${task.text}</li>`).join("")}
+        </ul>
+      </div>`
+
+      const update = {
+        add: (state, form) => {
+            // access data in a form
+            const data = new FormData(form)
+            const task = {
+                id: window.crypto.getRandomValues(new Uint8Array(3)).join(""),
+                text: data.get('task'),
+                status: 0
+            }
+            state.todotasks.push(task)
+            return state
+        }
+    }
+    
 
 app.start('KanbanProject', state, view, update)
